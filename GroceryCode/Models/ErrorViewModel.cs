78f5e1a9ba6cc735feac10b2ecac4ad8ea9dc7ca
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2023-05-11 18:47:26 -0700

    Add project files.

---
+++ GroceryCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GroceryCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
