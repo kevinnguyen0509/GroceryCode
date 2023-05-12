b87f884b0d501a02b52f2391a5c7dd0337295bc4
bpgor <brianado@msn.com>
2023-05-11 18:55:17 -0700

    Removed baja

--- GroceryCode/Controllers/HomeController.cs
+++ GroceryCode/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace GroceryCode.Controllers
 {
-    public class HomeController : Controller //test baja
+    public class HomeController : Controller //test
     {
         private readonly ILogger<HomeController> _logger;
 
